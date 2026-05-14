e950be862cafcf4d6ab6c6914110b9ed6aa44471
youngbb13 <153384410+youngbb13@users.noreply.github.com>
2026-05-14 17:31:20 +0200

    upgraded simple BankSystem

--- src/BankService.java
+++ src/BankService.java
@@ -1,9 +1,16 @@
+import java.util.List;
+import java.util.Map;
+
 public class BankService {
-    public void withdrawFromAll(Account[] accounts, double amount) {
+    public void withdrawFromAll(List<Account> accounts, double amount) {
         for(Account account : accounts) {
             account.showInfo();
             account.withdraw(amount);
             System.out.println();
         }
+    }
+
+    public Account findAccountByNumber(Map<String, Account> accounts, String accountNumber) {
+        return accounts.get(accountNumber);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         Account debitDima = new DebitAccount("Dima", 120500);
         Account creditValentyn = new CreditAccount("Valentyn", 40000, 50000);
         Account savingRuslan = new SavingAccount("Ruslan", 5000);
 
-        Account[] accounts = {debitDima, creditValentyn, savingRuslan};
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(debitDima);
+        accounts.add(creditValentyn);
+        accounts.add(savingRuslan);
 //        for(Account account : accounts) {
 //            account.showInfo();
 //            account.withdraw(2500);
@@ -14,6 +22,14 @@ public class Main {
         BankService bankService = new BankService();
         bankService.withdrawFromAll(accounts, 2500);
 
+        Map<String, Account> bankAccounts = new HashMap<>();
+        bankAccounts.put("ACC-001", debitDima);
+        bankAccounts.put("ACC-002", creditValentyn);
+        bankAccounts.put("ACC-003", savingRuslan);
+
+        Account found = bankService.findAccountByNumber(bankAccounts, "ACC-001");
+        found.showInfo();
+        System.out.println();
 
         Transferable transferableAndriy = new DebitAccount("Andriy", 300);
         transferableAndriy.transfer(creditValentyn, 100);
